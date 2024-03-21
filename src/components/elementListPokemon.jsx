3854b6c1b7ee3e65dd4682a18452e9c0d8a2a316
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2024-03-21 16:41:13 +0100

    Ajout des boutons de navigations et ajout de quelques css (#9)

--- src/components/elementListPokemon.jsx
+++ src/components/elementListPokemon.jsx
@@ -5,17 +5,17 @@ const ElementListPokemon = ({ id, name, types, image }) => {
     return (
         <Link 
             to={`/pokemon/${id}`}
-            className='row mt-1 w-75 m-auto'
+            className='row mt-1 width-row m-auto'
             style={{ textDecoration: ' none', color: 'white' }}
         >
-            <div className='col-2'>
+            <div className='col-3 col-lg-2'>
                 <img
                     src={image}
                     alt={name}
                 />
             </div>
-            <div className='col-2 my-auto'>{id}</div>
-            <div className='col-5 my-auto'>{name}</div>
+            <div className='col-1 col-lg-2 my-auto'>{id}</div>
+            <div className='col-4 col-lg-5 my-auto' style={{ textTransform: "Capitalize" }}>{name}</div>
             <div className='col-3 my-auto'>
                 <div className='row'>
                     {types.map((t, id) => {
