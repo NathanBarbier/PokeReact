003f646eada1d909c66daa4d84120d21ad0b8fd6
CorentinMaille <81628097+CorentinMaille@users.noreply.github.com>
2024-03-21 11:55:12 +0100

    omg un commit (#4)
    Co-authored-by: CorentinMaille <maille.corentin@pm.me>

--- src/routes/pokemons.jsx
+++ src/routes/pokemons.jsx
@@ -1,23 +1,29 @@
-/* eslint-disable no-restricted-globals */
+import { useLoaderData } from "react-router-dom";
 
-import { Form, useLoaderData } from "react-router-dom";
+export async function loader() {
+    const pokemons = await getPokemons();
+    return { pokemons };
+}
+
+export async function getPokemons() {
+  return null;
+}
 
 export default function Pokemons() {
+    const pokemons = useLoaderData();
 
-  const { contact } = useLoaderData();
-
-  return (
-    <div id="pokemons">
+    return (
+      <div>
+        <h1>Bienvenue sur le Pokedex !!!</h1>
         {/* TODO */}
-
-
+        
         {/* RECUPERER LES 20 PREMIERS POKEMONS AVEC L'API */}
-
+        
         {/* Pour chaque pokemon (loop), afficher le composant PokemonLine */}
-
+        
         {/* En bas de page ajouter une pagination pour les 20 prochains pokemons 
             Au click émettre une requête de récupération vers l'api
         */}
-    </div>
-  );
+      </div>
+    );
 }
